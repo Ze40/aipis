c36d4aad39db943a588a1eac8adf1fd28efb7ad4
Schcerbakov Maxim <miko.r4r@gmail.com>
2025-11-15 18:21:13 +0300

    docs: add commets

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string>
 
+// Главная функция - точка входа в программу
 int main() {
-    std::string name;
+    std::string name;  // Переменная для хранения имени пользователя
     
+    // Запрашиваем ввод от пользователя
     std::cout << "Enter your name: ";
-    std::cin >> name;
+    std::cin >> name;  // Читаем ввод пользователя
     
+    // Выводим приветственное сообщение
     std::cout << "Hello world from " << name << std::endl;
     
-    return 0;
+    return 0;  // Программа завершена успешно
 }
